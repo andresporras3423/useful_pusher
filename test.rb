134f7f00c4c8044e9952904cca51d127b71ac10d
andresporras3423 <andresporras3423@gmail.com>
2020-09-27 14:02:55 -0500

    optimize search method

--- test.rb
+++ test.rb
@@ -1,5 +1,8 @@
-lines = File.foreach("commits.txt").count
-num = rand(lines)
-commit = File.foreach("commits.txt").to_a[num]
-message = "git add . && git commit -m '#{commit}' && git push"
-system(message)
+loop do
+   lines = File.foreach("commits.txt").count
+   num = rand(lines)
+   commit = File.foreach("commits.txt").to_a[num]
+   message = "git add . && git commit -m '#{commit}' && git push"
+   system(message)
+   sleep(300)
+end
